cb89dc27bd6c749101c6c380fdd01c2bdf271fbe
Sarvarbek Salohiddinov <5ar7ar@mail.ru>
2024-11-17 19:40:44 +0500

    some clean up

--- spring-core/src/main/java/uz/sar7ar/springcore/config/ActiveMQConfig.java
+++ spring-core/src/main/java/uz/sar7ar/springcore/config/ActiveMQConfig.java
@@ -28,28 +28,4 @@ public class ActiveMQConfig {
         jmsTemplate.setDefaultDestinationName("defaultQueue");  // Set default destination here
         return jmsTemplate;
     }
-//    @Bean
-//    public MessageConverter jacksonJmsMessageConverter() {
-//        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-//        converter.setTargetType(MessageType.TEXT);  // Send messages as plain text (JSON)
-//        converter.setTypeIdPropertyName("_type");
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.registerModule(new JavaTimeModule());
-//        converter.setObjectMapper(objectMapper);
-//
-//        return converter;
-//    }
-//    @Bean
-//    public ConnectionFactory connectionFactory() {
-//        // Provide the ActiveMQ broker URL
-//        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
-//        factory.setBrokerURL("tcp://localhost:61616");
-//        return factory;
-//    }
-
-//    @Bean
-//    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
-//        return new JmsTemplate(connectionFactory);
-//    }
 }
